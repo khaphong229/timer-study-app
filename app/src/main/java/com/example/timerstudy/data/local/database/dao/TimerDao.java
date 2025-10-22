eaafb715a7a5aac7f7cac1520862f192e05c3149
MinhQuang0611 <nguyenminhquang061105@gmail.com>
2025-10-22 07:53:02 +0700

    [REF] refactor db code

--- app/src/main/java/com/example/timerstudy/data/local/database/dao/TimerDao.java
+++ app/src/main/java/com/example/timerstudy/data/local/database/dao/TimerDao.java
@@ -1,4 +1,4 @@
-package com.example.timerstudy.database;
+package com.example.timerstudy.data.local.database.dao;
 
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
@@ -6,6 +6,8 @@ import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
 import androidx.room.Update;
+
+import com.example.timerstudy.data.local.database.entities.TimerEntity;
 
 import java.util.List;
 
