1987f1beb5dd5306b96e75ede687c374f7e36a93
MinhDV-82 <f1minh88@gmail.com>
2025-12-15 16:39:22 +0700

    add constant layout landscape

--- app/src/main/java/com/example/timerstudy/view/activities/MainActivity.java
+++ app/src/main/java/com/example/timerstudy/view/activities/MainActivity.java
@@ -20,6 +20,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        // Hide system UI for full screen
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        decorView.setSystemUiVisibility(uiOptions);
+        
         setContentView(R.layout.activity_main);
 
         setupNavigation();
